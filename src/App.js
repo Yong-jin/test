eae8744c1de1df4f96163ac872d2e474cddf46e7
Yong-jin <takeardor@gmail.com>
2024-12-13 09:12:21 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+
+      <header>
+        <h1>This Pages Test</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
